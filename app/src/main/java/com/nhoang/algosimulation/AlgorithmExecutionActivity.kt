673c92780a1d98b6a2264b7ecadb1f17a5616ad0
NelsonH <nelson.hoang@gmail.com>
2024-12-04 11:38:53 -0800

    Update button style

--- app/src/main/java/com/nhoang/algosimulation/AlgorithmExecutionActivity.kt
+++ app/src/main/java/com/nhoang/algosimulation/AlgorithmExecutionActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.nhoang.algosimulation.ui.AlgorithmExecutionScreen
+import com.nhoang.algosimulation.ui.theme.AlgoSimulationTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AlgorithmExecutionActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            AlgorithmExecutionScreen()
+            AlgoSimulationTheme {
+                AlgorithmExecutionScreen()
+            }
         }
     }
 }
--- app/src/main/java/com/nhoang/algosimulation/ui/ProblemSelectorScreen.kt
+++ app/src/main/java/com/nhoang/algosimulation/ui/ProblemSelectorScreen.kt
@@ -1,18 +1,20 @@
 package com.nhoang.algosimulation.ui
 
 import android.content.Intent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.nhoang.algosimulation.AlgorithmExecutionActivity
 
 @Composable
@@ -23,11 +25,12 @@ fun ProblemSelectorScreen() {
             .wrapContentSize(Alignment.Center)
     ) {
         val context = LocalContext.current
-        TextButton(
+        OutlinedButton (
             onClick = {
                 context.startActivity(Intent(context, AlgorithmExecutionActivity::class.java))
             },
-            modifier = Modifier.background(Color.Gray)
+            border = BorderStroke(1.dp, Color.LightGray),
+            shape = RoundedCornerShape(50)
         ) {
             Text("Run Sub Array Sum Equals K")
         }
